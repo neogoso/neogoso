29dc6d5a1022ae2ac080d6cc339d56b9f6f6f0a8
Yihan Kim <kabi@kaist.ac.kr>
2016-06-07 00:19:23 +0900

    NaverNews iterator

--- NaverNews/crawler.py
+++ NaverNews/crawler.py
@@ -7,55 +7,78 @@ import time
 
 import sys
 reload(sys)
-sys.setdefaultencoding('utf-8')
-
-url = '''
-	https://apis.naver.com/commentBox/cbox5/web_naver_list_jsonp.json?
-	ticket=news&
-	templateId=default_ent&
-	_callback=&
-	lang=ko&
-	country=KR&
-	objectId=news109%2C0003328307&
-	pageSize=20&
-	indexSize=10&
-	page=1&
-	initialize=true&
-	useAltSort=true&
-	replyPageSize=100&
-	sort=new&
-	'''
-url = url.replace('\n','').replace('\t','').replace(' ','')
-
-request = urllib2.Request(url)
-request.add_header('host','apis.naver.com')
-request.add_header('referer','http://entertain.naver.com/comment/list?oid=109&aid=0003328307&gid=999339&cid=1017324')
-response = urllib2.urlopen(request)
-
-
-result = response.read()
-result = result[10:-2];
-
 import json
-json_acceptable_string = result.replace("'", "\"")
-d = json.loads(json_acceptable_string)
-print d['result']['count']
-print d['result']['pageModel']
+import re
 
-column ='''commentNo
-		contents
-		maskedUserId
-		modTime
-		modTimeGmt
-		objectId
-		parentCommentNo
-		profileUserId
-		regTime
-		regTimeGmt
-		userIdNo
-		userName'''.replace('\t','').replace(' ','').split('\n')
-print column
+def getComment(oid, aid):
 
+	sys.setdefaultencoding('utf-8')
+	total = 1000000; # can be modified
+	step = 20
+	cnt = 39
+	while(step * cnt < total):
+		cnt += 1
+		url = '''
+			https://apis.naver.com/commentBox/cbox5/web_naver_list_jsonp.json?
+			ticket=news&
+			templateId=default_ent&
+			_callback=&
+			lang=ko&
+			country=KR&
+			objectId=news109%2C0003328307&
+			pageSize=20&
+			indexSize=10&
+			page=1&
+			initialize=true&
+			useAltSort=true&
+			replyPageSize=100&
+			sort=new&
+			'''
+
+		url = url.replace('\n','').replace('\t','').replace(' ','')
+		url = url.replace("page=1", "page=%s" % str(cnt))
+		url = url.replace("pageSize=20", "pageSize=%s" % str(step))
+		url = url.replace("objectId=news109,0003328307", "objectId=news%s,%s" % (oid, aid))
+		
+
+		request = urllib2.Request(url)
+		request.add_header('host','apis.naver.com')
+		request.add_header('referer','http://entertain.naver.com/comment/list?oid=%s&aid=%s' % (oid, aid))
+		response = urllib2.urlopen(request)
+
+		result = response.read().replace('\\x', '\\u00')
+		result = result[10:-2];
+		json_acceptable_string = result.replace('"', "\"")
+
+		d = json.loads(json_acceptable_string)
+
+		for i in d['result']['commentList']:
+			print i['contents']
+		total = d['result']['count']['comment']
+	
+		print d['result']['pageModel']
+		
+
+	column ='''commentNo
+			contents
+			maskedUserId
+			modTime
+			modTimeGmt
+			objectId
+			parentCommentNo
+			profileUserId
+			regTime
+			regTimeGmt
+			userIdNo
+			userName'''.replace('\t','').replace(' ','').split('\n')
+
+	print column
+	return
+
+if __name__ == '__main__':
+	getComment('109', '0003328307')
+	
+'''
 t = time.time()
 twit = Twitter()
 
@@ -69,3 +92,5 @@ for comment in d['result']['commentList']:
 	
 
 print time.time() - t
+
+'''
